c67c0464a11b629702bfa5cbc8cb6ecad19d1e3a
Jordan Frimpter <52897732+Hikaito@users.noreply.github.com>
2023-01-29 19:24:52 -0600

    updates
    fixed class printing in dict

--- Project_1/main.py
+++ Project_1/main.py
@@ -2,51 +2,104 @@
 
 # Python Input Processing Project
 
-
-# main function for python project
 import os
+import pickle
 import sys
 import re
 
-class Person:
-    '''Data container class'''
-    def __init__(self, last, first, mi, id, phone):
-        '''Accepts the arguments for data containment and stores as variables
+# local file name for pickle file
+pickleFile = "out.p"
 
-        no error checking is performed because all data classes can be stored as strings'''
+class Person:
+    """data container class"""
+    def __init__(self, last, first, mi, id_in, phone):
+        """Accepts the arguments for data containment and stores as variables
+
+        no error checking is performed because all data classes can be stored as strings"""
         # store variables
         self.last = last
         self.first = first
         self.mi = mi
-        self.id = id
+        self.id_in = id_in
         self.phone = phone
 
     def __str__(self):
-        '''Builds internal information into display format and returns as a string
+        """Builds internal information into display format and returns as a string
 
         Permits values that aren't strings internally, as long as they can be cast to strings
         returns a string
-        '''
+        """
         # if middle initial has a value, include it in the string
         if self.mi:
-            return "Employee id: " + str(self.id) + "\n\t" + str(self.first) + " " + str(self.mi) + " " + str(
+            return "Employee id: " + str(self.id_in) + "\n\t" + str(self.first) + " " + str(self.mi) + " " + str(
                 self.last) + "\n\t" + str(self.phone)
         # if middle initial doesn't have a value, don't include it in the string
-        return "Employee id: " + str(self.id) + "\n\t" + str(self.first) + " " + str(self.last) + "\n\t" + str(
+        return "Employee id: " + str(self.id_in) + "\n\t" + str(self.first) + " " + str(self.last) + "\n\t" + str(
             self.phone)
 
+    def __repr__(self):
+        """returns call to __str__(self)"""
+        return self.__str__()
+
     def display(self):
-        '''Prints string representation of self to screen'''
-        print(self.__str__())
+        """Prints string representation of self to screen"""
+        print(self)
+
+def validateId(idCandidate):
+    """Generates a valid id
+    input: id string read from file
+    output: valid id, either from file or user"""
+
+    # typecheck id: if id is not a string, make it an empty string
+    if type(idCandidate) != str:
+        idCandidate = ""
+
+    while True:
+        # match ID to regex for content and length
+        match = re.search("^[a-zA-Z]{2}[0-9]{4}$", idCandidate)
+
+        # if there is a match to the regex, return uppercase version of the id string
+        if match:
+            return idCandidate.upper()
+
+        # if the token count for the ID is incorrect or the contents are not valid, reject
+        print("ID invalid: " + idCandidate)
+        print("ID is two letters followed by 4 digits")
+        print("Please enter a valid id:")
+        idCandidate = input()    # get new input from user
+
+def validatePhone(phone):
+    """Generates a valid phone number
+    Accepts any phone number with 10 numbers in a generally accepted phone number format, such as (000) - 000 - 0000 or 000.000.0000
+    in addition to 000-000-0000
+    input: string read from file
+    output: valid phone number, either from file or user"""
+
+    # typecheck id: if number is not a string, make it an empty string
+    if type(phone) != str:
+        phone = ""
+
+    while True:
+        # match if it resembles a phone number, capture the number groups for reformatting
+        match = re.search("^\(?\s*([0-9]{3})\s*[+.-]?\)?\s*[\s+.-]?\s*([0-9]{3})\s*[+.-]?\s*([0-9]{4})$", phone)
+
+        # if phone number was found, return phone number with proper format
+        if match:
+            return match.group(1) +  "-" + match.group(2) + "-" + match.group(3)
+
+        # if the token count is incorrect or the contents are not valid, reject
+        print("Phone " + phone + " is invalid")
+        print("Enter phone number in form 123-456-7890:")
+        phone = input()    # get new input from user
 
 def processInput(path):
-    '''Process input file function
+    """Process input file function
     inputs: a string path to an existing file.
         Expected format: first line is header, following lines have the pattern of
             Last,First,MiddleInitial,ID,OfficePhone
             or Last,First,ID,OfficePhone
     outputs: dictionary of People objects
-    '''
+    """
 
     people = {}
 
@@ -116,27 +169,10 @@ def processInput(path):
             tokens[2] = tokens[2][0].upper()
 
         # validate id----------------------------
-        # if the token count for the ID is incorrect, reject
-        if not len(tokens[3]) == 6:
-            print("Invalid id '" + tokens[3] + "'; rejecting line")
-            continue;
-        match = re.search("[a-zA-Z]{2}[0-9]{4}", tokens[3])
-        if not match:
-            print("Invalid id '" + tokens[3] + "'; rejecting line")
-            continue
-        # force uppercase
-        tokens[3] = tokens[3].upper()
+        tokens[3] = validateId(tokens[3])
 
         # validate phone----------------------------
-        # remove all whitespace
-        tokens[4] = "".join(tokens[4].split())
-        #FIIXME accepts improper number counts for some reason
-        match = re.search("[0-9]{3}[\s+.-]*[0-9]{3}[\s+.-]*[0-9]{4}", tokens[4])
-        if not match:
-            print("Invalid phone number '" + tokens[4] + "'; rejecting line")
-            continue
-        # if line has proper number set, rebuild phone number
-        tokens[4] = match.string[:3] + "-" + match.string[4:7] + "-" + match.string[8:]
+        tokens[4] = validatePhone(tokens[4])
 
         # if line was not rejected, create person object
         people[tokens[3]] = Person(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4])
@@ -144,7 +180,7 @@ def processInput(path):
     return people
 
 def main():
-    """Project 1
+    """Project_1
 
     Input expected as a sysarg; exits prematurely if absent
     """
@@ -175,6 +211,17 @@ def main():
     for person in people:
         people[person].display()
         print("\n")
+
+    print(people)
+
+    # pickle the file
+    pickle.dump(people, open(pickleFile, 'wb')) # save binary pickle file
+
+    # open the file
+    fromPickle = pickle.load(open(pickleFile, 'rb')) # read binary pickle file
+
+    # confirm file
+    print(fromPickle)
 
 # executes main function
 if __name__ == "__main__":
